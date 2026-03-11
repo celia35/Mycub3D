c332e2666b8905949414767b77767ac383a715e3
Celia Brice <cbrice@f6r13s1.clusters.42paris.fr>
2026-03-11 19:31:35 +0100

    test_open_window

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,6 +1,16 @@
 #ifndef CUB3D_H
 #define CUB3D_H
 
+typedef struct s_img{
+	void	*mlx_img;
+}t_img;
+
+typedef struct s_mlx{
+	void	*win;
+	void	*mlx;
+}t_mlx;
+
+
 typedef struct s_window
 {
 	void	*mlx;
